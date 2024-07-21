60722cb8f6863c2a14feadb5e6af09e86f435729
Aditya Sharma <adityaomsharma2004@gmail.com>
2024-07-21 13:49:05 +0530

    add contact

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -3,6 +3,7 @@ import React from "react";
 class AddContact extends React.Component {
     render() {
         return (
+         
             <div className="ui main">
                     <h2 >Add Contact</h2>
                 <form className="ui form">
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -9,15 +9,14 @@ const ContactList = (props) => {
                 <div className="content">
                     <div className="header">{contact.name}</div>
                     <div>{contact.email}</div>
+
                 </div>
+                <i className="trash alternate outline icon" ></i>
             </div>
         );
     });
 
-    return  <div className="ui celled list">
-            <h2>Contact List</h2>
-            {renderContactList}
-        </div>
+    return <div className="ui celled list">{renderContactList}</div>
 
 }
 
